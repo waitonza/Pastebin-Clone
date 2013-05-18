7963ff86a7bb043c024221eca92e95d0d8a07dc6
Wairung Tiranalinvit <wairung3421@gmail.com>
2013-05-19 00:15:23 +0700

    Init Project

---
+++ README.md
@@ -0,0 +1,16 @@
+Paste Bin (Clone) For Software Testing Course
+====================
+### Purpose : 
+
+Mini Project For Software Testing Course
+Starting Test with TDD/BDD
+
+### Developemnt Info :
+
+- Ruby on Rails 3.2.13
+- Cucumber + RSpec (For Testing Framework)
+
+### Members :
+
+Mr. Wairung Tiranalinvit 		5310545620
+Mr. Punmonkol Chutcharinkul 	5310546481
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PatebinClone::Application.config.secret_key_base = 'e4b02aef77f84e8621ccdd766f8afbd50b0a781616f83f9689b9441750b5fdf0efeb924ff32b600a41dd77a67d6f01dd0b47cbcea1934a6b594e9f47160dcfa8'
